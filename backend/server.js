78ab11b8fb19a5223ed019533e1da4205866d8c7
Noah Sedlik <noahsedlik@berkeley.edu>
2022-04-13 16:31:22 -0700

    fix(backend): move app.listen to end of file

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,19 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+/* ENDPOINTS */
+app.get("/", (req, res) => {
+    res.send("Hello!");
+})
+
+app.get("/api/match/:userID", (req, res) => {
+    const userID = parseInt(req.params.userID);
+    res.send(userID);
+})
+
 /* Start the server */
 app.use(express.json());
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
-})
-
-app.get("/", (req, res) => {
-    res.send("Hello!");
-})
-
-/* ENDPOINTS */
-app.get("/api/match/:userID", (req, res) => {
-    const userID = parseInt(req.params.userID);
-    res.send(userID);
 })
