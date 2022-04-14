3bde341627049ac8e0a3056654e6cc9c0a9091b8
Noah Sedlik <noahsedlik@berkeley.edu>
2022-04-13 17:10:26 -0700

    feat(server.js): add match and profile test functionality

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const { randomInt } = require('crypto');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,12 +10,16 @@ app.get("/", (req, res) => {
 
 app.get("/api/match/:userID", (req, res) => {
     // TODO -- run the matching algorithm
+    res.send([ 
+        {"userID": testUser.userID, "matchScore": randomInt(9) + 1},
+        {"userID": testUser.userID * 2, "matchScore": randomInt(9) + 1},
+    ]);
 })
 
 app.get("/api/profile/:targetID", (req, res) => {
     const targetID = parseInt(req.params.targetID);
-    getProfile(targetID);
-    res.send(targetID);
+    const profile = getProfile(targetID);
+    res.send(profile);
     
 })
 
@@ -26,6 +31,30 @@ app.listen(port, (req, res) => {
 })
 
 function getProfile(targetID) {
-    assert(isNaN(targetID));
+    if(isNaN(targetID)) {
+        throw new Error("ahh");
+    }
     // TODO - make request to database and return userprofile JSON
+    return testUser;
+}
+
+const testUser = {
+    "name": "testuser",
+    "userID": 12345,
+    "pictureURL": "https://tinder.com/static/tinder.png",
+    "data": {
+        "cleanliness": 5,
+        "morningPerson": true,
+        "nightPerson": false,
+        "smokingTolerance": false,
+        "housing": {
+            "unit1": true,
+            "unit2": false,
+            "unit3": true,
+            "foothill": false,
+            "clarkKerr": true,
+            "martinez": false,
+            "blackwell": true,
+        }
+    }
 }
