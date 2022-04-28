ac777a1ae9ea7da99d7014078d04868168b6efd0
Kaihao Wen <kaiwen129@berkeley.edu>
2022-04-28 16:33:17 -0700

    save

--- frontend/src/Components/Result.js
+++ frontend/src/Components/Result.js
@@ -19,14 +19,35 @@ import {
 import Card from "./Card";
 import Header from './Header.js'
 import About from "./About";
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
+const Result=(props)=>{
+    const id = props.id; //id of the user whose results should be displayed
+    const [data, setData] = useState();
+    const getData = () => {
+      axios
+          .get(`http://localhost:3002/api/match/${id}`)
+          .then((data) => setData(data.data))
+          .catch((error) => console.log(error));
+    };
+    
+    useEffect(() => {
+      getData();
+    }, []);
+    
+    const ints = [1,2,3,4,5,6,7];
 
-const Result=()=>{
     return( 
     <div>
         <Header/>
-        <Flex width="100%" justifyContent="center">
-            <Card id="1"></Card>  
+        <Flex width="100%" justifyContent="space-evenly" flexWrap="wrap">
+            {
+              ints.map(function(int){
+                return <Card id={4166207}></Card>
+                //person["user"][1]["userID"]
+              })
+            } 
         </Flex>
         <About/>
   </div>)
